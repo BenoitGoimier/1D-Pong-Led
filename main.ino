3ed89032ceae00ab5cb7ec8104114b80c3ac5159
Benoit G <benoitgoimier@gmail.com>
2025-01-05 14:55:27 +0100

    Update main.ino

--- main.ino
+++ main.ino
@@ -14,27 +14,29 @@
 #define VX_BASE 0.4                // Base speed of the Pong ball
 #define SPEED_BALL_MIN 0.15        // Minimum speed of the Pong ball
 #define SPEED_BALL_MAX 2           // Maximum speed of the Pong ball
+#define SPEED_BALL_INCREMENT 0.1  // Increment of the Pong ball speed at each hit
 #define PLAYER_DISABLED_DELAY 500  // Delay in ms during which a player's button is disabled after hitting the ball
 #define VICTORY_DELAY 3000         // Delay in ms for victory animation
 #define DEFEAT_DELAY 2000          // Delay in ms for defeat animation
 #define SCORE_MAX 5                // Maximum score to win a game
 #define ANIMATION_SPEED 75         // Speed of animations in the choice menu (lower is faster)
 #define CTF_BASE_SPEED 40         // Initial speed of the ships (delay in ms between movements)
-#define CTF_SPEED_INCREMENT 5     // Amount of speed increase (decrease in delay) when a point is captured
+#define CTF_SPEED_INCREMENT 8     // Amount of speed increase (decrease in delay) when a point is captured
 
 CRGB leds[NUM_LEDS];  // Array to store LED colors
 
 // Game modes
 enum GameMode {
-  CHOICE,   // Game selection menu
-  PONG,     // Pong game
-  REFLEXE,  // Reflex game
-  PUSH,     // Push game
-  CTF       // Capture The Flag game
+  CHOICE,     // Game selection menu
+  PONG,       // Pong game
+  PONGSPEED,  // Pong game with increasing speed
+  REFLEXE,    // Reflex game
+  PUSH,       // Push game
+  CTF         // Capture The Flag game
 };
 
-GameMode gameMode = CTF;  // Current game mode
-int selectedGame = 0;     // Index of the currently selected game in the choice menu (0: Pong, 1: Reflexe, 2: Push)
+GameMode gameMode = CHOICE;  // Current game mode
+int selectedGame = 0;       // Index of the currently selected game in the choice menu (0: Pong, 1: PongSpeed, 2: Reflexe, 3: Push, 4: CTF)
 
 bool gameSetup = false;  // Flag to indicate if the current game has been set up
 
@@ -146,6 +148,9 @@ void loop() {
     case PONG:
       pongLoop();  // Run the Pong game
       break;
+    case PONGSPEED:
+      pongSpeedLoop(); // Run the PongSpeed game
+      break;
     case REFLEXE:
       reflexeLoop();  // Run the Reflex game
       break;
@@ -170,7 +175,7 @@ void choiceLoop() {
   // Handle input from Player 1's button (change game selection)
   if (digitalRead(PLAYER1_PIN) == LOW) {
     handleButtonPress(PLAYER1_PIN);
-    selectedGame = (selectedGame + 1) % 4;  // Cycle through 0, 1, 2, 3
+    selectedGame = (selectedGame + 1) % 5;  // Cycle through 0, 1, 2, 3, 4
   }
 
   // Handle input from Player 2's button (confirm selection)
@@ -197,12 +202,15 @@ void selectGame() {
       gameMode = PONG;
       break;
     case 1:
-      gameMode = REFLEXE;
+      gameMode = PONGSPEED;
       break;
     case 2:
-      gameMode = PUSH;
+      gameMode = REFLEXE;
       break;
     case 3:
+      gameMode = PUSH;
+      break;
+    case 4:
       gameMode = CTF;
       break;
   }
@@ -221,13 +229,16 @@ void animateChoice() {
     case 0:  // Pong
       animatePong(animationOffset, 30);
       break;
-    case 1:  // Reflexe
+    case 1:  // Pong Speed
+      animatePongSpeed(animationOffset, 30);
+      break;
+    case 2:  // Reflexe
       animateReflexe(animationOffset, 30);
       break;
-    case 2:  // Push
+    case 3:  // Push
       animatePush(animationOffset, 30);
       break;
-    case 3:  // Capture the Flag
+    case 4:  // Capture the Flag
       animateCaptureTheFlag(animationOffset, 30);
       break;
   }
@@ -252,6 +263,25 @@ void animatePong(int offset, int length) {
   }
 
   leds[offset + pongBallX] = CRGB::White;
+}
+
+// Function to animate the PongSpeed game in the choice menu
+void animatePongSpeed(int offset, int length) {
+  static int pongSpeedBallX = 0;  // Current X position of the "ball"
+  static int pongSpeedDir = 1;    // Current direction of the "ball"
+  static int pongSpeedVX = 2;
+
+  // Draw "rackets"
+  leds[offset] = CRGB::Blue;
+  leds[offset + length - 1] = CRGB::Red;
+
+  // Move the "ball"
+  pongSpeedBallX += pongSpeedDir * pongSpeedVX;
+  if (pongSpeedBallX <= 0 || pongSpeedBallX >= length - 2) {
+    pongSpeedDir *= -1;  // Reverse direction when hitting an edge
+  }
+
+  leds[offset + pongSpeedBallX] = CRGB::White;
 }
 
 // Function to animate the Reflex game in the choice menu
@@ -485,13 +515,13 @@ void pongSetup() {
 
   // Display countdown animation
   FastLED.clear();
-  fill_solid(leds + NUM_LEDS / 2 - 2, 4, ball.color);
+  fill_solid(leds + NUM_LEDS / 2 - 2, 4, CRGB::Red);
   FastLED.show();
   delay(600);
-  fill_solid(leds + NUM_LEDS / 2 - 4, 8, ball.color);
+  fill_solid(leds + NUM_LEDS / 2 - 4, 8, CRGB::Yellow);
   FastLED.show();
   delay(600);
-  fill_solid(leds + NUM_LEDS / 2 - 6, 12, ball.color);
+  fill_solid(leds + NUM_LEDS / 2 - 6, 12, CRGB::Green);
   FastLED.show();
   delay(600);
 
@@ -518,6 +548,200 @@ void player2Win() {
   resetSet();
 }
 
+// ---------------------- PONGSPEED GAME -----------------------------
+
+// Function to run the PongSpeed game loop
+void pongSpeedLoop() {
+  if (!gameSetup) {
+    pongSpeedSetup();  // Initialize the PongSpeed game
+  }
+
+  FastLED.clear();  // Clear the LED strip
+
+  // Handle player input and update racket positions
+  handlePlayerInputPongSpeed();
+
+  // Display game elements
+  displayGameElementsPongSpeed();
+
+  // Update ball position and check for collisions
+  updateBallPositionPongSpeed();
+
+  FastLED.show();  // Update the LED strip
+}
+
+// Function to handle player input for the PongSpeed game
+void handlePlayerInputPongSpeed() {
+  // Handle input from Player 1
+  if (digitalRead(PLAYER1_PIN) == LOW && millis() > player1.disabledEndTime && !player1.press) {
+    player1.press = true;
+    if (ball.x >= RACKET_LENGTH) {
+      player1.disabledEndTime = millis() + PLAYER_DISABLED_DELAY;
+    } else {
+      // Increase ball speed on each hit
+      ball.vx += SPEED_BALL_INCREMENT;
+      // Limit ball speed to the maximum speed
+      ball.vx = constrain(ball.vx, VX_BASE, SPEED_BALL_MAX);
+      ball.dx = 1;
+      ball.color = player1.color;
+    }
+  }
+
+  // Handle input from Player 2
+  if (digitalRead(PLAYER2_PIN) == LOW && millis() > player2.disabledEndTime && !player2.press) {
+    player2.press = true;
+    if (ball.x < NUM_LEDS - RACKET_LENGTH) {
+      player2.disabledEndTime = millis() + PLAYER_DISABLED_DELAY;
+    } else {
+      // Increase ball speed on each hit
+      ball.vx += SPEED_BALL_INCREMENT;
+      // Limit ball speed to the maximum speed
+      ball.vx = constrain(ball.vx, VX_BASE, SPEED_BALL_MAX);
+      ball.dx = -1;
+      ball.color = player2.color;
+    }
+  }
+
+  // Reset button press flags when buttons are released
+  if (digitalRead(PLAYER1_PIN) == HIGH) {
+    player1.press = false;
+  }
+  if (digitalRead(PLAYER2_PIN) == HIGH) {
+    player2.press = false;
+  }
+}
+
+// Function to display game elements for the PongSpeed game
+void displayGameElementsPongSpeed() {
+  // Display Player 1's racket if not disabled
+  if (millis() > player1.disabledEndTime) {
+    fill_gradient_RGB(leds, 0, player1.raketColor1, RACKET_LENGTH - 1, player1.raketColor2);
+  }
+
+  // Display Player 2's racket if not disabled
+  if (millis() > player2.disabledEndTime) {
+    fill_gradient_RGB(leds, NUM_LEDS - RACKET_LENGTH, player2.raketColor1, NUM_LEDS, player2.raketColor2);
+  }
+
+  // Display other game elements (ball, scores, disabled rackets)
+  for (int i = 0; i < NUM_LEDS; i++) {
+    if (i == (int)ball.x) {
+      leds[i] = ball.color;  // Draw the ball
+    } else if (i < NUM_LEDS / 2 && i >= NUM_LEDS / 2 - player1.score) {
+      leds[i] = CRGB::Green;  // Draw Player 1's score
+    } else if (i >= NUM_LEDS / 2 && i < NUM_LEDS / 2 + player2.score) {
+      leds[i] = CRGB::Yellow;  // Draw Player 2's score
+    } else if (i < RACKET_LENGTH && millis() < player1.disabledEndTime) {
+      leds[i] = player1.colorDisabled;  // Draw Player 1's disabled racket
+    } else if (i >= NUM_LEDS - RACKET_LENGTH && millis() < player2.disabledEndTime) {
+      leds[i] = player2.colorDisabled;  // Draw Player 2's disabled racket
+    }
+  }
+}
+
+// Function to update ball position for the PongSpeed game
+void updateBallPositionPongSpeed() {
+  // Check for collisions with the edges
+  if (ball.x <= 0 || ball.x >= NUM_LEDS) {
+    if (ball.x >= NUM_LEDS) {
+      player1WinPongSpeed();  // Player 1 scores
+    } else {
+      player2WinPongSpeed();  // Player 2 scores
+    }
+  }
+
+  // Move the ball
+  ball.x += ball.dx * ball.vx;
+}
+
+// Function to reset the ball for the PongSpeed game
+void resetSetPongSpeed() {
+  ball.x = RACKET_LENGTH;
+  ball.color = player1.color;
+
+  if (ball.dx == 1) {
+    ball.x = NUM_LEDS - RACKET_LENGTH - 1;
+    ball.color = player2.color;
+  }
+
+  ball.vx = VX_BASE;  // Reset to base speed
+  ball.dx *= -1;      // Reverse direction
+
+  // Check if a player has reached the maximum score
+  if (player1.score >= SCORE_MAX || player2.score >= SCORE_MAX) {
+    gameSetup = false;  // Reset game setup to start a new game
+  }
+}
+
+// Function to set up the PongSpeed game
+void pongSpeedSetup() {
+  // Check if a player has won the previous game
+  if (player1.score >= SCORE_MAX) {
+    unsigned long loopStart = millis();
+    // Victory animation for player 1
+    while (millis() - loopStart < VICTORY_DELAY) {
+      fill_rainbow(leds, NUM_LEDS / 2, (millis() - loopStart));
+      FastLED.show();
+    }
+  } else if (player2.score >= SCORE_MAX) {
+    unsigned long loopStart = millis();
+    // Victory animation for player 2
+    while (millis() - loopStart < VICTORY_DELAY) {
+      fill_rainbow(leds + NUM_LEDS / 2, NUM_LEDS / 2, (millis() - loopStart));
+      FastLED.show();
+    }
+  } else {
+    // Start direction
+    ball.x = 1;
+    ball.dx = 1;
+    if (rand() % 2 == 1) {
+      ball.x = NUM_LEDS - 1;
+      ball.dx = -1;
+    }
+  }
+
+  // Reset scores
+  player1.score = 0;
+  player2.score = 0;
+
+  // Reset speed
+  ball.vx = VX_BASE;
+
+  // Display countdown animation
+  FastLED.clear();
+  fill_solid(leds + NUM_LEDS / 2 - 2, 4, CRGB::Red);
+  FastLED.show();
+  delay(600);
+  fill_solid(leds + NUM_LEDS / 2 - 4, 8, CRGB::Yellow);
+  FastLED.show();
+  delay(600);
+  fill_solid(leds + NUM_LEDS / 2 - 6, 12, CRGB::Green);
+  FastLED.show();
+  delay(600);
+
+  gameSetup = true;  // Set the setup flag
+}
+
+// Function to handle player 1 scoring a point in PongSpeed game
+void player1WinPongSpeed() {
+  player1.score++;  // Increment player 1's score
+  // Point scored animation
+  fill_rainbow_circular(leds + NUM_LEDS / 3, NUM_LEDS / 6, 100, false);
+  FastLED.show();
+  delay(1000);
+  resetSetPongSpeed();
+}
+
+// Function to handle player 2 scoring a point in PongSpeed game
+void player2WinPongSpeed() {
+  player2.score++;  // Increment player 2's score
+  // Point scored animation
+  fill_rainbow_circular(leds + NUM_LEDS / 2, NUM_LEDS / 6, 100, true);
+  FastLED.show();
+  delay(1000);
+  resetSetPongSpeed();
+}
+
 // ---------------------- REFLEXE GAME -----------------------------
 
 unsigned long start = 0;  // Variable to store the start time of the Reflex game
@@ -528,7 +752,7 @@ void reflexeLoop() {
     reflexSetup();  // Initialize the Reflex game
   }
 
-  unsigned long delaiReaction = millis() - start;  // Calculate the player's reaction time
+  unsigned long delaiReaction = max(9, millis() - start);  // Calculate the player's reaction time
 
   // Check if the reaction time exceeds the maximum allowed time
   if ((delaiReaction / 3 >= NUM_LEDS)) {
@@ -545,7 +769,7 @@ void reflexeLoop() {
     fill_solid(leds, delaiReaction / 3, player1.color);
     FastLED.show();
 
-    delay(5000);        // Wait for 5 seconds
+    delay(VICTORY_DELAY);        // Wait for 5 seconds
     gameSetup = false;  // Reset setup flag
     return;             // Exit the loop
   }
@@ -558,7 +782,7 @@ void reflexeLoop() {
     fill_solid(leds + NUM_LEDS - delaiReaction / 3, delaiReaction / 3, player2.color);
     FastLED.show();
 
-    delay(5000);        // Wait for 5 seconds
+    delay(VICTORY_DELAY);        // Wait for 5 seconds
     gameSetup = false;  // Reset setup flag
     return;             // Exit the loop
   }
@@ -569,22 +793,31 @@ void reflexSetup() {
   FastLED.clear();  // Turn off all LEDs
 
   // Display countdown animation
-  fill_solid(leds + NUM_LEDS / 2 - 2, 4, CRGB::Green);
+  fill_solid(leds + NUM_LEDS / 2 - 2, 4, CRGB::Blue);
   FastLED.show();
-  delay(rand() % 200 + 400);
-  fill_solid(leds + NUM_LEDS / 2 - 4, 8, CRGB::Green);
+  delay(300);
+  fill_solid(leds + NUM_LEDS / 2 - 4, 8, CRGB::Red);
   FastLED.show();
-  delay(rand() % 200 + 400);
-  fill_solid(leds + NUM_LEDS / 2 - 6, 12, CRGB::Green);
+  delay(300);
+  fill_solid(leds + NUM_LEDS / 2 - 6, 12, CRGB::Yellow);
   FastLED.show();
-  delay(rand() % 200 + 400);
+  delay(300);
   fill_solid(leds + NUM_LEDS / 2 - 8, 16, CRGB::Green);
   FastLED.show();
 
-  delay(rand() % 2000 + 500);
+  int wait = rand() % 5000;
+  Serial.println(wait);
+  delay(wait);
 
   FastLED.clear();  // Turn off all LEDs
-  FastLED.show();   // Update LEDs
+  FastLED.show();
+
+
+  if (digitalRead(PLAYER1_PIN) == LOW || digitalRead(PLAYER2_PIN) == LOW) {
+    reflexToEarly();     // Player loses
+    gameSetup = false;  // Reset setup flag
+    return;             // Exit the loop
+  }
 
   start = millis();  // Record the start time
   gameSetup = true;  // Set the setup flag
@@ -600,10 +833,20 @@ void reflexDefeat() {
     if ((millis() - loopStart) / 300 % 2 == 1) {
       reversed = false;
     }
-    fill_rainbow_circular(leds + 30, NUM_LEDS / 2, (millis() - loopStart), reversed);
+    fill_rainbow_circular(leds + 30, NUM_LEDS - 60, (millis() - loopStart), reversed);
     FastLED.show();
   }
-  gameMode = CHOICE;  // Go back to the game selection
+}
+
+// Function to handle player defeat in the Reflex game
+void reflexToEarly() {
+  unsigned long loopStart = millis();
+  bool reversed = false;
+  // Defeat animation
+  while (millis() - loopStart < DEFEAT_DELAY) {
+    fill_solid(leds + 30, NUM_LEDS - 60, CRGB::DarkRed);
+    FastLED.show();
+  }
 }
 
 // ---------------------- Push GAME -----------------------------
@@ -629,13 +872,13 @@ void handlePushGameInput() {
   // Handle input from Player 1
   if (digitalRead(PLAYER1_PIN) == LOW && !player1.press) {
     player1.press = true;
-    ball.x++;  // Move the ball to the right
+    ball.x += 5;  // Move the ball to the right
   }
 
   // Handle input from Player 2
   if (digitalRead(PLAYER2_PIN) == LOW && !player2.press) {
     player2.press = true;
-    ball.x--;  // Move the ball to the left
+    ball.x -= 5;  // Move the ball to the left
   }
 
   // Reset button press flags when buttons are released
@@ -667,6 +910,19 @@ void checkPushGameWinConditions() {
 // Function to set up the Push game
 void pushSetup() {
   ball.x = NUM_LEDS / 2;  // Reset ball position to the center
+
+  // Display countdown animation
+  FastLED.clear();
+  fill_solid(leds + NUM_LEDS / 2 - 2, 4, CRGB::Red);
+  FastLED.show();
+  delay(600);
+  fill_solid(leds + NUM_LEDS / 2 - 4, 8, CRGB::Yellow);
+  FastLED.show();
+  delay(600);
+  fill_solid(leds + NUM_LEDS / 2 - 6, 12, CRGB::Green);
+  FastLED.show();
+  delay(600);
+
   gameSetup = true;       // Set the setup flag
 }
 
@@ -681,7 +937,7 @@ void player1WinLoop() {
     fill_rainbow_circular(leds, NUM_LEDS / 6, (millis() - loopStart), false);
     FastLED.show();
   }
-  gameMode = CHOICE;  // Go back to the game selection
+  gameSetup = false;       // Set the setup flag
 }
 
 // Function to handle player 2 winning a round in the Push game
@@ -695,7 +951,7 @@ void player2WinLoop() {
     fill_rainbow_circular(leds + NUM_LEDS - NUM_LEDS / 6, NUM_LEDS / 6, (millis() - loopStart), true);
     FastLED.show();
   }
-  gameMode = CHOICE;  // Go back to the game selection
+  gameSetup = false;       // Set the setup flag
 }
 
 // ---------------------- CAPTURE THE FLAG GAME -----------------------------
@@ -742,37 +998,39 @@ void ctfSetup() {
 }
 
 void handleCtfInput() {
-  // Handle input from Player 1
-  if (digitalRead(PLAYER1_PIN) == LOW && !player1.press) {
-    player1.press = true;
-    if (ship1.position == flagPosition) {
-      player2.score = max(0, ++player1.score);
-      flagCaptured = true;
-    } else {
-      player1.score = player1.score - 1;  // Prevent score from going below 0
+    // Handle input from Player 1
+    if (digitalRead(PLAYER1_PIN) == LOW && !player1.press) {
+        player1.press = true;
+        // Check if ship1 is at, one position before, or one position after flagPosition
+        if (ship1.position >= flagPosition - 1 && ship1.position <= flagPosition + 1) {
+            player1.score++;
+            flagCaptured = true;
+        } else {
+            player1.score = max(0, player1.score - 1); // Decrement score, but not below 0
+        }
     }
-    ship1.speed = CTF_BASE_SPEED - player1.score * CTF_SPEED_INCREMENT; 
-  }
+    ship1.speed = CTF_BASE_SPEED - player1.score * CTF_SPEED_INCREMENT; // Increase speed
 
-  // Handle input from Player 2
-  if (digitalRead(PLAYER2_PIN) == LOW && !player2.press) {
-    player2.press = true;
-    if (ship2.position == flagPosition) {
-      player2.score = max(0, ++player2.score);
-      flagCaptured = true;
-    } else {
-      player2.score = player2.score - 1;  // Prevent score from going below 0
+    // Handle input from Player 2
+    if (digitalRead(PLAYER2_PIN) == LOW && !player2.press) {
+        player2.press = true;
+        // Check if ship2 is at, one position before, or one position after flagPosition
+        if (ship2.position >= flagPosition - 1 && ship2.position <= flagPosition + 1) {
+            player2.score++;
+            flagCaptured = true;
+        } else {
+            player2.score = max(0, player2.score - 1); // Decrement score, but not below 0
+        }
     }
-    ship2.speed = CTF_BASE_SPEED - player2.score * CTF_SPEED_INCREMENT;
-  }
+    ship2.speed = CTF_BASE_SPEED - player2.score * CTF_SPEED_INCREMENT; // Increase speed
 
-  // Reset button press flags when buttons are released
-  if (digitalRead(PLAYER1_PIN) == HIGH) {
-    player1.press = false;
-  }
-  if (digitalRead(PLAYER2_PIN) == HIGH) {
-    player2.press = false;
-  }
+    // Reset button press flags when buttons are released
+    if (digitalRead(PLAYER1_PIN) == HIGH) {
+        player1.press = false;
+    }
+    if (digitalRead(PLAYER2_PIN) == HIGH) {
+        player2.press = false;
+    }
 }
 
 void moveShips() {
@@ -869,5 +1127,4 @@ void ctfGameOver(int winner) {
     delay(500);
   }
   gameSetup = false;  // Reset setup flag
-  gameMode = CHOICE;  // Go back to the game selection
 }
